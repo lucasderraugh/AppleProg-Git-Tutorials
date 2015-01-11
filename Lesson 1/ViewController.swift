034762a1b3c92be6ac8c9397007860dfca674e8e
Lucas Derraugh <lucasderraugh@gmail.com>
2015-01-11 02:30:41 -0500

    Initial commit

---
+++ Lesson 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson 1
+//
+//  Created by Lucas Derraugh on 1/11/15.
+//  Copyright (c) 2015 Lucas Derraugh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
